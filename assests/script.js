7537afa693cba90d11ec89922b45da8f503dc9b2
Matthew Cross <matthewcross1017@gmail.com>
2022-05-23 11:06:04 -0400

    delete speak function and start Speech path

--- assests/script.js
+++ assests/script.js
@@ -7,39 +7,18 @@ const patientName = document.querySelector(".patientname");
 let patientText = patientName.outerText.toString();
 const question = document.querySelectorAll(".question");
 let result = [];
+let voices = [];
 let boundary = document.querySelector(".next");
 let paragraphs = document.querySelectorAll("p");
 let nextButton = document.querySelector(".proxyNext");
 let newQuestion = document.querySelector(".newQuestion");
 
-const questionObject = {
-  utterance1: new SpeechSynthesisUtterance(question[0].textContent),
-  utterance2: new SpeechSynthesisUtterance(question[1].textContent),
-  utterance3: new SpeechSynthesisUtterance(question[2].textContent),
-  utterance3: new SpeechSynthesisUtterance(question[3].textContent),
-  utterance4: new SpeechSynthesisUtterance(question[4].textContent),
-  utterance5: new SpeechSynthesisUtterance(question[5].textContent),
-  utterance6: new SpeechSynthesisUtterance(question[6].textContent),
-  utterance7: new SpeechSynthesisUtterance(question[7].textContent),
-  utterance8: new SpeechSynthesisUtterance(question[8].textContent),
-  utterance9: new SpeechSynthesisUtterance(question[9].textContent),
-  utterance10: new SpeechSynthesisUtterance(question[10].textContent),
-  utterance11: new SpeechSynthesisUtterance(question[11].textContent),
-  utterance12: new SpeechSynthesisUtterance(question[12].textContent),
-  utterance13: new SpeechSynthesisUtterance(question[13].textContent),
-  utterance14: new SpeechSynthesisUtterance(question[14].textContent),
-  utterance15: new SpeechSynthesisUtterance(question[15].textContent),
-  utterance16: new SpeechSynthesisUtterance(question[16].textContent),
-  utterance17: new SpeechSynthesisUtterance(question[17].textContent),
-  utterance18: new SpeechSynthesisUtterance(question[18].textContent),
-  utterance19: new SpeechSynthesisUtterance(question[19].textContent),
-  utterance20: new SpeechSynthesisUtterance(question[20].textContent),
-  utterParagraph: new SpeechSynthesisUtterance(paragraphs[20].textContent),
-  utterParagraph2: new SpeechSynthesisUtterance(paragraphs[21].textContent),
-  utterParagraph3: new SpeechSynthesisUtterance(paragraphs[22].textContent),
-};
-console.log(questionObject);
 
+
+window.speechSynthesis.addEventListener('voiceschanged', function(){
+  voices = window.speechSynthesis.getVoices();
+  console.log(voices)
+})
 function highlightContent() {
   let highlightArray = [];
   for (let i = 0; i < paragraphs.length; i++) {
@@ -48,7 +27,6 @@ function highlightContent() {
       paragraphs[i].classList.add("mark");
     }
   }
-  console.log(highlightArray);
 }
 
 var utterThis = new SpeechSynthesisUtterance(question[0].textContent);
@@ -62,53 +40,47 @@ console.log(paragraphs);
 // Pass through html element to function so that it can read it 
 // Also have highlight function so it highlights at same time and same text
 
-function speak() {
-  if (synth.speaking) {
-    console.error("speechSynthesis.speaking");
-    return;
-  }
-  utterThis.onstart = function (event) {
-  
-  };
+const speakAll = (text) => {
+  return new Promise(resolve => {
+  const speech = new SpeechSynthesisUtterance(text)
+  speech.voice = voices.find(voice => voice.lang === 'en-US')
+  speech.rate = 1
+  window.speechSynthesis.speak(speech)
+  speech.addEventListener('start', ()=> {
+    console.log('start speaking')
+  });
 
-  if (utterThis.value !== "") {
-    console.log(utterThis);
-    console.log(utterQueue)
-    utterThis.onstart = function (event) {
-      let pending = synth.pending;
-      console.log("The Queue is " + pending);
-      highlightContent()
-      console.log(
-        "We have started uttering this speech: " + event.utterance.text
-      );
-    };
-  }
-  utterThis.onend = function (event) {
-    console.log("SpeechSynthesisUtterance.onend" + event.elapsedTime);
-  };
-  utterThis.onerror = function (event) {
-    console.error("SpeechSynthesisUtterance.onerror");
-  };
+  speech.addEventListener('end', ()=> {
+    console.log('stopped speaking')
+    resolve()
+  });
+});
+};
+
+const playSpeech = () => {
+  speakAll(question[0].textContent).then(() => {
+    console.log('first question completed')
+    speakAll(question[1].textContent)
+  })
 }
-speak();
 
 playButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  synth.speak(utterThis);
-  console.log(utterThis);
-});
-
-newQuestion.addEventListener("click", function (event) {
+  playSpeech()
   highlightContent()
   event.preventDefault();
-  synth.speak(utterQueue);
+  })
+
+newQuestion.addEventListener("click", function (event) {
+
+  event.preventDefault();
+ speakAll(question[1].textContent)
 });
 
-nextButton.addEventListener('touchstart', function(event){
-  event.preventDefault;
-  synth.speak(utterQueue);
-  window.alert('touch event works');
-});
+// nextButton.addEventListener('touchstart', function(event){
+//   event.preventDefault;
+//   synth.speak(utterQueue);
+//   window.alert('touch event works');
+// });
 
 
 
