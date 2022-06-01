d686c7f25f5518e41d605fec37f78fa9015574c9
Matthew Cross <matthewcross1017@gmail.com>
2022-05-31 20:09:41 -0400

    merge request

--- assests/script.js
+++ assests/script.js
@@ -18,57 +18,57 @@ console.log(question)
 
 
 
-function highlightContent(){
+// function highlightContent(){
 
-// First let's get all text nodes in the page.
-const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
-const allTextNodes = [];
-console.log(treeWalker)
-let currentNode = treeWalker.nextNode();
-while (currentNode) {
-  // There may also be hidden text nodes in the page
-  // like text inside a <script> tag. So ignore those.
-  if (getComputedStyle(currentNode.parentNode).display !== 'none') {
-    allTextNodes.push(currentNode);
-  }
-  currentNode = treeWalker.nextNode();
-}
-console.log(allTextNodes)
+// // First let's get all text nodes in the page.
+// const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+// const allTextNodes = [];
+// console.log(treeWalker)
+// let currentNode = treeWalker.nextNode();
+// while (currentNode) {
+//   // There may also be hidden text nodes in the page
+//   // like text inside a <script> tag. So ignore those.
+//   if (getComputedStyle(currentNode.parentNode).display !== 'none') {
+//     allTextNodes.push(currentNode);
+//   }
+//   currentNode = treeWalker.nextNode();
+// }
+// console.log(allTextNodes)
 
-// Then, loop through them, every time splitting them into
-// individual words, and creating a list of words per node.
-const allWords = [];
-for (const textNode of allTextNodes) {
-  for (const word of textNode.textContent.matchAll(/[a-zA-Z]+/g)) {
-    allWords.push({
-      word: word[0],
-      parentNode: textNode,
-      offset: word.index
-    });
-  }
-}
+// // Then, loop through them, every time splitting them into
+// // individual words, and creating a list of words per node.
+// const allWords = [];
+// for (const textNode of allTextNodes) {
+//   for (const word of textNode.textContent.matchAll(/[a-zA-Z]+/g)) {
+//     allWords.push({
+//       word: word[0],
+//       parentNode: textNode,
+//       offset: word.index
+//     });
+//   }
+// }
 
-// Finally, loop through the words and highlight them one by
-// one by creating a Range and Selection object.
-let index = 0;
-const range = new Range();
+// // Finally, loop through the words and highlight them one by
+// // one by creating a Range and Selection object.
+// let index = 0;
+// const range = new Range();
 
 
 
-setInterval(() => {
-  if (index >= allWords.length) {
-    index = 0;
-  }
-  const {word, parentNode, offset} = allWords[index];
-  // parentNode, offset original range
-  console.log(parentNode);
-  range.setStart(parentNode, offset);
-  range.setEnd(parentNode, offset + word.length);
-  document.getSelection().removeAllRanges();
-  document.getSelection().addRange(range);
-  index++;
-}, 325);
-}
+// setInterval(() => {
+//   if (index >= allWords.length) {
+//     index = 0;
+//   }
+//   const {word, parentNode, offset} = allWords[index];
+//   // parentNode, offset original range
+//   console.log(parentNode);
+//   range.setStart(parentNode, offset);
+//   range.setEnd(parentNode, offset + word.length);
+//   document.getSelection().removeAllRanges();
+//   document.getSelection().addRange(range);
+//   index++;
+// }, 325);
+// }
 
 window.speechSynthesis.addEventListener('voiceschanged', function () {
   voices = window.speechSynthesis.getVoices();
@@ -91,12 +91,11 @@ const speakAll = (text) => {
     // Add notification if api is already speaking
     // });    
     speech.addEventListener('boundary', (event) => {
-      console.log(event.charIndex, event.name)
-      let e = document.getElementByClass('mark')
+      console.log(event.charIndex, event.name, event.utterance)
+
     })
     speech.addEventListener('end', () => {
       console.log('stopped speaking')
-      window.speechSynthesis.pause()
       resolve()
     });
   });
@@ -129,7 +128,6 @@ const playSpeech = async () => {
 
 playButton.addEventListener("click", function (event) {
   playSpeech();
-   highlightContent();
   event.preventDefault();
 })
 
