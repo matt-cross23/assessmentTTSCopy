b1ed51c39d0c14fe4ca01181d2794d5bc7eff877
Matthew Cross <matthewcross1017@gmail.com>
2022-06-08 17:25:53 -0400

    merge

--- assests/script.js
+++ assests/script.js
@@ -20,23 +20,25 @@ let globalWords = [];
 let pageText = $('body').text().trim().replace(/\n/g, '')
 console.log(pageText)
 
-
-for(let i = 0; i < question.length; i++){
-result.push(question[i].textContent)
-
+// Highlight pt 2 
+const highlightBackground = sample => `<span style="background-color:yellow;">${sample}</span>`
+const highlight = (text, from, to) => {
+  let replacement = highlightBackground(text.slice(from, to))
+  return text.substring(0, from) + replacement + text.substring(to)
 }
-console.log(result)
 
 
 window.speechSynthesis.addEventListener('voiceschanged', function () {
   voices = window.speechSynthesis.getVoices();
-  console.log(voices)
+  // console.log(voices)
 })
-let originalText = pageText
+
 
 // Update speech function to be less specific by 
 // Pass through html element to function so that it can read it 
 // Also have highlight function so it highlights at same time and same text
+let text = document.getElementById("text");
+let originalText = text.innerText;
 
 const speakAll = (text) => {
   return new Promise(resolve => {
@@ -44,7 +46,6 @@ const speakAll = (text) => {
     speech.voice = voices.find(voice => voice.lang === 'en-US')
     speech.rate = .75
     window.speechSynthesis.speak(speech) 
-
 speech.addEventListener('end', () => {
       console.log('stopped speaking')
       // window.speechSynthesis.pause()
@@ -54,11 +55,7 @@ speech.addEventListener('end', () => {
       let words = e.target.text.substr(e.charIndex).match(/^.+?\b/)[0]
       console.log(words)
   });
-  const highlightBackground = sample => `<span style="background-color:yellow;">${sample}</span>`
-  const highlight = (text, from, to) => {
-    let replacement = highlightBackground(text.slice(from, to))
-    return text.substring(0, from) + replacement + text.substring(to)
-  }
+
     speech.addEventListener('boundary', (event) => {
       const { charIndex, charLength } = event
       text.innerHTML = highlight(originalText, charIndex, charIndex + charLength)
@@ -76,10 +73,7 @@ const playSpeech = async () => {
 playButton.addEventListener("click", function (event) {
   event.preventDefault();
    playSpeech();
-   let text = pageText.split(' ');
-   globalWords = text
-   console.log(globalWords)
-
+   console.log(text)
 })
 
 
