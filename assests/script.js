05c19fec503cb7d9a58e72fed3d1dca583f55338
Matthew Cross <matthewcross1017@gmail.com>
2022-06-28 14:27:56 -0400

    finished pitch and rate

--- assests/script.js
+++ assests/script.js
@@ -7,13 +7,102 @@
 // drop down of voices and sliding scale for rate globally for Demo 
 // Read and highlight individually question and once done reading alert const t0 = performance.now();
 console.log("TTS Script connected");
-let synth = window.speechSynthesis
-var voices = synth.getVoices();
 let result = [];
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   console.log(voices)
 };
+// For rate and pitch
+var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector("form");
+var inputTxt = document.querySelector(".txt");
+var voiceSelect = document.querySelector("select");
+
+var pitch = document.querySelector("#pitch");
+var pitchValue = document.querySelector(".pitch-value");
+var rate = document.querySelector("#rate");
+var rateValue = document.querySelector(".rate-value");
+
+var voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase(),
+      bname = b.name.toUpperCase();
+    if (aname < bname) return -1;
+    else if (aname === bname) return 0;
+    else return +1;
+  });
+  var selectedIndex =
+    voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = "";
+  for (let i = 0; i < voices.length; i++) {
+    var option = document.createElement("option");
+    option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+
+    if (voices[i].default) {
+      option.textContent += " -- DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+document.querySelector('#play').addEventListener('click', function speak() {
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+    var utterThis = new SpeechSynthesisUtterance('Hello World, This is a text to speech script. Thank you for your time');
+    utterThis.onend = function (event) {
+      console.log("SpeechSynthesisUtterance.onend");
+    };
+    utterThis.onerror = function (event) {
+      console.error("SpeechSynthesisUtterance.onerror");
+    };
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute(
+      "data-name"
+    );
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    utterThis.pitch = pitch.value;
+    utterThis.rate = rate.value;
+    synth.speak(utterThis);
+})
+
+inputForm.onsubmit = function (event) {
+  event.preventDefault();
+
+  speak();
+
+  inputTxt.blur();
+};
+
+pitch.onchange = function () {
+  pitchValue.textContent = pitch.value;
+};
+
+rate.onchange = function () {
+  rateValue.textContent = rate.value;
+};
+
+voiceSelect.onchange = function () {
+  speak();
+};
+
 
 // Pause
 $("[pause]").on("click", () => {
