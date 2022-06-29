5b05666004d2d665402f78e9c027866ecc60e751
Matthew Cross <matthewcross1017@gmail.com>
2022-06-28 20:57:40 -0400

    add rate/pitch with utterance

--- assests/script.js
+++ assests/script.js
@@ -8,100 +8,95 @@
 // Read and highlight individually question and once done reading alert const t0 = performance.now();
 console.log("TTS Script connected");
 let result = [];
-window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
-  console.log(voices)
-};
+let synth = window.speechSynthesis;
+// window.speechSynthesis.onvoiceschanged = () => {
+//   voices = window.speechSynthesis.getVoices();
+//   console.log(voices)
+// };
 // For rate and pitch
-var synth = window.speechSynthesis;
+// var synth = window.speechSynthesis;
 
-var inputForm = document.querySelector("form");
-var inputTxt = document.querySelector(".txt");
-var voiceSelect = document.querySelector("select");
+// var inputForm = document.querySelector("form");
+// var inputTxt = document.querySelector(".txt");
+// var voiceSelect = document.querySelector("select");
 
 var pitch = document.querySelector("#pitch");
 var pitchValue = document.querySelector(".pitch-value");
 var rate = document.querySelector("#rate");
 var rateValue = document.querySelector(".rate-value");
 
-var voices = [];
+// var voices = [];
 
-function populateVoiceList() {
-  voices = synth.getVoices().sort(function (a, b) {
-    const aname = a.name.toUpperCase(),
-      bname = b.name.toUpperCase();
-    if (aname < bname) return -1;
-    else if (aname === bname) return 0;
-    else return +1;
-  });
-  var selectedIndex =
-    voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-  voiceSelect.innerHTML = "";
-  for (let i = 0; i < voices.length; i++) {
-    var option = document.createElement("option");
-    option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+// function populateVoiceList() {
+//   voices = synth.getVoices().sort(function (a, b) {
+//     const aname = a.name.toUpperCase(),
+//       bname = b.name.toUpperCase();
+//     if (aname < bname) return -1;
+//     else if (aname === bname) return 0;
+//     else return +1;
+//   });
+//   var selectedIndex =
+//     voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+//   voiceSelect.innerHTML = "";
+//   for (let i = 0; i < voices.length; i++) {
+//     var option = document.createElement("option");
+//     option.textContent = voices[i].name + " (" + voices[i].lang + ")";
 
-    if (voices[i].default) {
-      option.textContent += " -- DEFAULT";
-    }
+//     if (voices[i].default) {
+//       option.textContent += " -- DEFAULT";
+//     }
 
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    voiceSelect.appendChild(option);
-  }
-  voiceSelect.selectedIndex = selectedIndex;
-}
+//     option.setAttribute("data-lang", voices[i].lang);
+//     option.setAttribute("data-name", voices[i].name);
+//     voiceSelect.appendChild(option);
+//   }
+//   voiceSelect.selectedIndex = selectedIndex;
+// }
 
-populateVoiceList();
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = populateVoiceList;
-}
+// populateVoiceList();
+// if (speechSynthesis.onvoiceschanged !== undefined) {
+//   speechSynthesis.onvoiceschanged = populateVoiceList;
+// }
 
-document.querySelector('#play').addEventListener('click', function speak() {
-  if (synth.speaking) {
-    console.error("speechSynthesis.speaking");
-    return;
-  }
-    var utterThis = new SpeechSynthesisUtterance('Hello World, This is a text to speech script. Thank you for your time');
-    utterThis.onend = function (event) {
-      console.log("SpeechSynthesisUtterance.onend");
-    };
-    utterThis.onerror = function (event) {
-      console.error("SpeechSynthesisUtterance.onerror");
-    };
-    var selectedOption = voiceSelect.selectedOptions[0].getAttribute(
-      "data-name"
-    );
-    for (let i = 0; i < voices.length; i++) {
-      if (voices[i].name === selectedOption) {
-        utterThis.voice = voices[i];
-        break;
-      }
-    }
-    utterThis.pitch = pitch.value;
-    utterThis.rate = rate.value;
-    synth.speak(utterThis);
-})
+// document.querySelector('#play').addEventListener('click', function speak() {
+//   if (synth.speaking) {
+//     console.error("speechSynthesis.speaking");
+//     return;
+//   }
+//     var utterThis = new SpeechSynthesisUtterance('Hello World, This is a text to speech script. Thank you for your time');
+//     utterThis.onend = function (event) {
+//       console.log("SpeechSynthesisUtterance.onend");
+//     };
+//     utterThis.onerror = function (event) {
+//       console.error("SpeechSynthesisUtterance.onerror");
+//     };
+//     var selectedOption = voiceSelect.selectedOptions[0].getAttribute(
+//       "data-name"
+//     );
+//     for (let i = 0; i < voices.length; i++) {
+//       if (voices[i].name === selectedOption) {
+//         utterThis.voice = voices[i];
+//         break;
+//       }
+//     }
+//     utterThis.pitch = pitch.value;
+//     utterThis.rate = rate.value;
+//     synth.speak(utterThis);
+// })
 
-inputForm.onsubmit = function (event) {
-  event.preventDefault();
+// inputForm.onsubmit = function (event) {
+//   event.preventDefault();
 
-  speak();
+//   speak();
 
-  inputTxt.blur();
-};
+//   inputTxt.blur();
+// };
 
-pitch.onchange = function () {
-  pitchValue.textContent = pitch.value;
-};
 
-rate.onchange = function () {
-  rateValue.textContent = rate.value;
-};
 
-voiceSelect.onchange = function () {
-  speak();
-};
+// voiceSelect.onchange = function () {
+//   speak();
+// };
 
 
 // Pause
@@ -156,7 +151,15 @@ $('[play]').on('click', function (event) {
       readblockElement.html(readBlockText)
     })
     // utterance.voice = 1
-    utterance.voice = voices[0]
+    pitch.onchange = function () {
+  pitchValue.textContent = pitch.value;
+};
+
+rate.onchange = function () {
+  rateValue.textContent = rate.value;
+};
+    utterance.pitch = pitch.value;
+    utterance.rate = rate.value;
     synth.speak(utterance);
   });
 })
