2c93983c235b5f14779c46ea38c6c6d94feb1a24
Matthew Cross <matthewcross1017@gmail.com>
2022-06-29 00:07:03 -0400

    highlight only english and change voices

--- assests/script.js
+++ assests/script.js
@@ -18,79 +18,54 @@ let synth = window.speechSynthesis;
 
 // var inputForm = document.querySelector("form");
 // var inputTxt = document.querySelector(".txt");
-// var voiceSelect = document.querySelector("select");
+const voiceSelect = document.querySelector("select");
 
-var pitch = document.querySelector("#pitch");
-var pitchValue = document.querySelector(".pitch-value");
-var rate = document.querySelector("#rate");
-var rateValue = document.querySelector(".rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector(".pitch-value");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector(".rate-value");
 
-// var voices = [];
+let voices = [];
 
-// function populateVoiceList() {
-//   voices = synth.getVoices().sort(function (a, b) {
-//     const aname = a.name.toUpperCase(),
-//       bname = b.name.toUpperCase();
-//     if (aname < bname) return -1;
-//     else if (aname === bname) return 0;
-//     else return +1;
-//   });
-//   var selectedIndex =
-//     voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-//   voiceSelect.innerHTML = "";
-//   for (let i = 0; i < voices.length; i++) {
-//     var option = document.createElement("option");
-//     option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase();
+    const bname = b.name.toUpperCase();
 
-//     if (voices[i].default) {
-//       option.textContent += " -- DEFAULT";
-//     }
+    if (aname < bname) {
+      return -1;
+    } else if (aname == bname) {
+      return 0;
+    } else {
+      return +1;
+    }
+  });
+  const selectedIndex =
+    voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = "";
 
-//     option.setAttribute("data-lang", voices[i].lang);
-//     option.setAttribute("data-name", voices[i].name);
-//     voiceSelect.appendChild(option);
-//   }
-//   voiceSelect.selectedIndex = selectedIndex;
-// }
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-// populateVoiceList();
-// if (speechSynthesis.onvoiceschanged !== undefined) {
-//   speechSynthesis.onvoiceschanged = populateVoiceList;
-// }
+    if (voices[i].default) {
+      option.textContent += " -- DEFAULT";
+    }
 
-// document.querySelector('#play').addEventListener('click', function speak() {
-//   if (synth.speaking) {
-//     console.error("speechSynthesis.speaking");
-//     return;
-//   }
-//     var utterThis = new SpeechSynthesisUtterance('Hello World, This is a text to speech script. Thank you for your time');
-//     utterThis.onend = function (event) {
-//       console.log("SpeechSynthesisUtterance.onend");
-//     };
-//     utterThis.onerror = function (event) {
-//       console.error("SpeechSynthesisUtterance.onerror");
-//     };
-//     var selectedOption = voiceSelect.selectedOptions[0].getAttribute(
-//       "data-name"
-//     );
-//     for (let i = 0; i < voices.length; i++) {
-//       if (voices[i].name === selectedOption) {
-//         utterThis.voice = voices[i];
-//         break;
-//       }
-//     }
-//     utterThis.pitch = pitch.value;
-//     utterThis.rate = rate.value;
-//     synth.speak(utterThis);
-// })
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
 
-// inputForm.onsubmit = function (event) {
-//   event.preventDefault();
+populateVoiceList();
 
-//   speak();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
 
-//   inputTxt.blur();
-// };
+
 
 
 
@@ -158,8 +133,17 @@ $('[play]').on('click', function (event) {
 rate.onchange = function () {
   rateValue.textContent = rate.value;
 };
+const selectedOption =
+voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+for (let i = 0; i < voices.length; i++) {
+if (voices[i].name === selectedOption) {
+  utterance.voice = voices[i];
+  break;
+}
+}
     utterance.pitch = pitch.value;
     utterance.rate = rate.value;
     synth.speak(utterance);
-  });
+  })
 })
